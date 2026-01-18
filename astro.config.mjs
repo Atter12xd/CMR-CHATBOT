85b072975985508d76fdb4aea96fd677de9c03f3
Atter12xd <1485275@senati.pe>
2026-01-17 20:53:07 -0500

    fsfçg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
+  output: 'hybrid', // Permite páginas estáticas y SSR
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   server: {
     port: 4321
